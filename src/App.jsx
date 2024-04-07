8d5ff0739f6dfd1cd398310e0fe9dca81f2c4752
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-04-07 12:46:15 +0530

    fixed some bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Grid from './Component/Grid/Grid';
 
 function App() {
   return (
-    <>
+    <div style={{ display: 'flex', alignItems: "center", justifyContent: "center" }}>
       <Grid numberOfCards={9} />
-    </>
+    </div>
   )
 }
 
--- src/Component/Grid/Grid.jsx
+++ src/Component/Grid/Grid.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Card from "../Card/Card";
 import isWinner from "../Helper/CheckWinner";
-// import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 
 import './Grid.css';
 // import 'react-toastify/dist/ReactToastify.css';
@@ -10,16 +10,16 @@ function Grid({ numberOfCards }) {
     const [turn, setTurn] = useState(true); // false -> X, true => 0
     const [board, setBoard] = useState(Array(numberOfCards).fill("")); // ["", "", "", "", "", ""]
     const [winner, setWinner] = useState(null);
-    
+
     function play(index) {
         console.log("move played", index);
-        if(turn == true) {
+        if (turn == true) {
             board[index] = "O";
         } else {
             board[index] = "X";
         }
         const win = isWinner(board, turn ? "O" : "X");
-        if(win) {
+        if (win) {
             setWinner(win);
             toast.success(`Congratulation ${win} win the game!!`)
         }
@@ -38,12 +38,12 @@ function Grid({ numberOfCards }) {
         <div className="grid-wrapper">
             {winner && (
                 <>
-                    <h1 className="turn-highlight"> Winner is {winner} </h1>
+                    <h1 className="turnHighlight"> Winner is {winner} </h1>
                     <button className="reset" onClick={reset}>Reset game</button>
                     <ToastContainer position="top-center" />
                 </>
             )}
-            <h1 className="turn-highlight">Current Turn: {(turn) ? 'O' : 'X'} </h1>
+            <h1 className="turnHighlight">Current Turn: {(turn) ? 'O' : 'X'} </h1>
             <div className="grid">
                 {board.map((value, idx) => {
                     return <Card gameEnd={winner ? true : false} onPlay={play} player={value} key={idx} index={idx} />
